9d76651b7a7e1b03cc2cb19d4c9f381d319a7c96
Javier Alcántara <39445008+javieralcantara@users.noreply.github.com>
2018-05-20 00:03:06 +0200

    Delete RandomNumber.js

--- RandomNumberContract/RandomNumber.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-$("#header").load('header.html');        
-$("#footer").load('footer.html');    
-
-// Global variables used by our Dapp
-var contract_address = "n1jj1zvHD1eCaeHUmAxaCtHteLxLoPQLVzm";
