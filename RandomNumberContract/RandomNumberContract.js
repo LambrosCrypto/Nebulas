8035d99b1c1f784bd8d43c3706f4bcd0710030a0
Javier Alcántara <javieralcantaraugr@gmail.com>
2018-05-20 13:38:15 +0200

    change

--- RandomNumberContract/RandomNumberContract.js
+++ RandomNumberContract/RandomNumberContract.js
@@ -17,7 +17,7 @@ RandonNumberContract.prototype = {
   // init is called once, when the contract is deployed.
   init: function() { },
 
-  function requestNumber(min, max) {
+  requestNumber: function (min, max) {
     if(Blockchain.transaction.value != 0) // Users only pay the gas fee.
         throw new Error("I don't want your money.");
 
