517b9a15118aaa0f9649c972538b2296c48df8d4
euss95 <eugenioalcantara1995@gmail.com>
2018-05-21 23:50:46 +0200

    Borrar
    Borrado comentarios.

--- RandomNumberContract2.5/RandomNumber.js
+++ RandomNumberContract2.5/RandomNumber.js
@@ -1,7 +1,4 @@
 "use strict";
-
-$("#header").load('header.html');        
-$("#footer").load('footer.html');    
-
+ 
 // Global variables used by our Dapp
 var contract_address = "n1jj1zvHD1eCaeHUmAxaCtHteLxLoPQLVzm";
