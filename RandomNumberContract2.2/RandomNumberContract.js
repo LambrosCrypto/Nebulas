c50b773452073db3145c3293b40eca1ba7a5ebdf
Javi <javieralcantaraugr@gmail.com>
2018-05-21 15:33:12 +0200

    No minimum now
    CHANGES TO DO:
    ABOUT.HTML
    RESULTS.HTML

--- RandomNumberContract2.2/RandomNumberContract.js
+++ RandomNumberContract2.2/RandomNumberContract.js
@@ -10,22 +10,22 @@
 
 var RandonmNumberContract = function() {
   // Data stored by the smart contract
-  LocalContractStorage.defineMapProperty(this, "hash_to_rnc") // Min, Max
+  LocalContractStorage.defineMapProperty(this, "hash_to_rnc") // 0, Max
 }
 
 RandonNumberContract.prototype = {
   // init is called once, when the contract is deployed.
   init: function() { },
 
-  requestNumber: function (min, max) {
+  requestNumber: function (0, max) {
     if(Blockchain.transaction.value != 0) // Users only pay the gas fee.
         throw new Error("I don't want your money.");
 
     if(isNaN(max) || max < 1) //Proving max is a number.
       throw new Error("max is not a number.");
 
-    var number = Math.floor(Math.random() * (max - min + 1) ) + min; //Random number min -> max both included.
-    this.hash_to_rnc.put(Blockchain.transaction.hash, {max, min, number, date: Date.now()});
+    var number = Math.floor(Math.random() * max; //Random number 0 -> max both included.
+    this.hash_to_rnc.put(Blockchain.transaction.hash, {max, 0, number, date: Date.now()});
   },
 
   getNumber: function (hash) {
